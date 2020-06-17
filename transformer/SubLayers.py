aaf3d9eace15bc99f0f5ef77661a9ea9153c06a2
Tae Jin Park <taejinpa@usc.edu>
2020-06-17 13:46:11 -0700

    self_attn_memory

--- transformer/SubLayers.py
+++ transformer/SubLayers.py
@@ -2,9 +2,86 @@
 import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
-from transformer.Modules import ScaledDotProductAttention
+# from transformer.Modules import ScaledDotProductAttention
+from Modules import ScaledDotProductAttention
+import ipdb
 
 __author__ = "Yu-Hsiang Huang"
+class MultiHeadAttentionMemory(nn.Module):
+    ''' Multi-Head Attention module '''
+
+    def __init__(self, n_head, d_model, d_k, d_v, dropout=0.1):
+        super().__init__()
+
+        self.n_head = n_head
+        self.d_k = d_k
+        self.d_v = d_v
+        
+        self.w_qs = nn.Linear(d_model, n_head * d_k, bias=False)
+        self.w_ks = nn.Linear(d_model, n_head * d_k, bias=False)
+        self.w_vs = nn.Linear(d_model, n_head * d_v, bias=False)
+        self.fc = nn.Linear(n_head * d_v, d_model, bias=False)
+
+        self.attention = ScaledDotProductAttention(temperature=d_k ** 0.5)
+
+        self.dropout = nn.Dropout(dropout)
+        self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
+
+
+    def forward(self, _q, k, v, mask=None):
+
+        d_k, d_v, n_head = self.d_k, self.d_v, self.n_head
+        sz_b, len_q, len_k, len_v = _q.size(0), _q.size(1), k.size(1), v.size(1)
+
+        residual = _q
+
+        # Pass through the pre-attention projection: b x lq x (n*dv)
+        # Separate different heads: b x lq x n x dv
+        # ipdb.set_trace()
+        _q = self.w_qs(_q).view(sz_b, len_q, n_head, d_k)
+        k = self.w_ks(k).view(sz_b, len_k, n_head, d_k)
+        v = self.w_vs(v).view(sz_b, len_v, n_head, d_v)
+
+        # Transpose for attention dot product: b x n x lq x dv
+        _q, k, v = _q.transpose(1, 2), k.transpose(1, 2), v.transpose(1, 2)
+
+        if mask is not None:
+            mask = mask.unsqueeze(1)   # For head axis broadcasting.
+
+        _q, attn = self.attention(_q, k, v, mask=mask)
+
+        # Transpose to move the head dimension back: b x lq x n x dv
+        # Combine the last two dimensions to concatenate all the heads together: b x lq x (n*dv)
+        _q = _q.transpose(1, 2).contiguous().view(sz_b, len_q, -1)
+        _q = self.dropout(self.fc(_q))
+        _q += residual
+
+        _q = self.layer_norm(_q)
+
+        return _q, attn
+
+
+class PositionwiseFeedForward(nn.Module):
+    ''' A two-feed-forward-layer module '''
+
+    def __init__(self, d_in, d_hid, dropout=0.1):
+        super().__init__()
+        self.w_1 = nn.Linear(d_in, d_hid) # position-wise
+        self.w_2 = nn.Linear(d_hid, d_in) # position-wise
+        self.layer_norm = nn.LayerNorm(d_in, eps=1e-6)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+
+        residual = x
+
+        x = self.w_2(F.relu(self.w_1(x)))
+        x = self.dropout(x)
+        x += residual
+
+        x = self.layer_norm(x)
+
+        return x
 
 class MultiHeadAttention(nn.Module):
     ''' Multi-Head Attention module '''
@@ -15,7 +92,7 @@ class MultiHeadAttention(nn.Module):
         self.n_head = n_head
         self.d_k = d_k
         self.d_v = d_v
-
+        
         self.w_qs = nn.Linear(d_model, n_head * d_k, bias=False)
         self.w_ks = nn.Linear(d_model, n_head * d_k, bias=False)
         self.w_vs = nn.Linear(d_model, n_head * d_v, bias=False)
@@ -28,6 +105,7 @@ class MultiHeadAttention(nn.Module):
 
 
     def forward(self, q, k, v, mask=None):
+        _q = q
 
         d_k, d_v, n_head = self.d_k, self.d_v, self.n_head
         sz_b, len_q, len_k, len_v = q.size(0), q.size(1), k.size(1), v.size(1)
@@ -36,6 +114,7 @@ class MultiHeadAttention(nn.Module):
 
         # Pass through the pre-attention projection: b x lq x (n*dv)
         # Separate different heads: b x lq x n x dv
+        # ipdb.set_trace()
         q = self.w_qs(q).view(sz_b, len_q, n_head, d_k)
         k = self.w_ks(k).view(sz_b, len_k, n_head, d_k)
         v = self.w_vs(v).view(sz_b, len_v, n_head, d_v)
--- transformer/unit_test.py
+++ transformer/unit_test.py
@@ -2,27 +2,56 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 import numpy as np
+import argparse
+
+from SubLayers import MultiHeadAttention, PositionwiseFeedForward
+from SubLayers import MultiHeadAttentionMemory
 
 
-from transformer.SubLayers import MultiHeadAttention, PositionwiseFeedForward
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-epoch', type=int, default=10)
+parser.add_argument('-b', '--batch_size', type=int, default=2048)
+
+parser.add_argument('-d_model', type=int, default=28*28)
+parser.add_argument('-d_inner_hid', type=int, default=2048)
+parser.add_argument('-d_k', type=int, default=64)
+parser.add_argument('-d_v', type=int, default=64)
+
+parser.add_argument('-n_head', type=int, default=8)
+parser.add_argument('-n_layers', type=int, default=6)
+parser.add_argument('-warmup','--n_warmup_steps', type=int, default=4000)
+parser.add_argument('-dropout', type=float, default=0.1)
 
 
-slf_attn = MultiHeadAttention(n_head, d_model, d_k, d_v, dropout=dropout)
+opt = parser.parse_args()
+opt.cuda = True
+
+device = torch.device('cuda' if opt.cuda else 'cpu')
 
 
-class EncoderLayer(nn.Module):
-    ''' Compose with two layers '''
+slf_attn = MultiHeadAttentionMemory(opt.n_head, opt.d_model, opt.d_k, opt.d_v, dropout=opt.dropout).cuda()
 
-    def __init__(self, d_model, d_inner, n_head, d_k, d_v, dropout=0.1):
-        super(EncoderLayer, self).__init__()
-        self.slf_attn = MultiHeadAttention(n_head, d_model, d_k, d_v, dropout=dropout)
-        self.pos_ffn = PositionwiseFeedForward(d_model, d_inner, dropout=dropout)
+# ebd_input = torch.randn(opt.batch_size, opt.d_mode, opt.n_head, opt.d_kl).cuda()
+length_ebd = 1
+ebd_input = torch.randn(opt.batch_size, length_ebd, opt.d_model).cuda()
 
-    def forward(self, enc_input, slf_attn_mask=None):
-        enc_output, enc_slf_attn = self.slf_attn(
-            enc_input, enc_input, enc_input, mask=slf_attn_mask)
-        enc_output = self.pos_ffn(enc_output)
-        return enc_output, enc_slf_attn
+repeat = 10
+for _ in range(repeat):
+    ebd_input = slf_attn(ebd_input, ebd_input, ebd_input)
+# class EncoderLayer(nn.Module):
+    # ''' Compose with two layers '''
+
+    # def __init__(self, d_model, d_inner, n_head, d_k, d_v, dropout=0.1):
+        # super(EncoderLayer, self).__init__()
+        # self.slf_attn = MultiHeadAttention(n_head, d_model, d_k, d_v, dropout=dropout)
+        # self.pos_ffn = PositionwiseFeedForward(d_model, d_inner, dropout=dropout)
+
+    # def forward(self, enc_input, slf_attn_mask=None):
+        # enc_output, enc_slf_attn = self.slf_attn(
+            # enc_input, enc_input, enc_input, mask=slf_attn_mask)
+        # enc_output = self.pos_ffn(enc_output)
+        # return enc_output, enc_slf_attn
 
 
 ########## DEBUG: unit test code ##########
